e6d2ee4367137cd5e4d43ecb03781ccbf9d40b85
SundayOf <alex.ryabikin04@gmail.com>
2021-03-21 19:51:59 +0300

    03/21/2021:19:51

--- Obscurity/core/src/com/obscurity/game/MainMenu.java
+++ Obscurity/core/src/com/obscurity/game/MainMenu.java
@@ -2,24 +2,26 @@ package com.obscurity.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.input.GestureDetector;
 import com.badlogic.gdx.maps.objects.RectangleMapObject;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.utils.viewport.StretchViewport;
 
 
 public class MainMenu extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img;
 	TiledMap map;
 	Stage stage;
 	int[] ground;
@@ -27,16 +29,25 @@ public class MainMenu extends ApplicationAdapter {
 	Rectangle[] bounds;
 	OrthogonalTiledMapRenderer render;
 	ShapeRenderer sr;
+	Player pp;
+	float gravity;
+	Boolean isonground;
+	OrthographicCamera cam;
 	@Override
 	public void create () {
+		isonground = false;
+		gravity = 0;
+		cam = new OrthographicCamera(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		cam.position.x = Gdx.graphics.getWidth()/2;
+		cam.position.y = Gdx.graphics.getHeight()/2;
+		cam.zoom = 0.25f;
 		batch = new SpriteBatch();
 		sr = new ShapeRenderer();
-		map = new TmxMapLoader().load("Cave.tmx");
+		map = new TmxMapLoader().load("map/Cave.tmx");
 		stage = new Stage(new StretchViewport(Gdx.graphics.getWidth(), Gdx.graphics.getHeight()));
 		ground = new int[1];
 		ground[0] = map.getLayers().getIndex("Ground");
 		render = new OrthogonalTiledMapRenderer(map);
-
 		int tmp = map.getLayers().get("Player").getObjects().getByType(RectangleMapObject.class).size;
 		for (int i=0;i<tmp;i++){
 			String stmp = (String)map.getLayers().get("Player").getObjects().getByType(RectangleMapObject.class).get(i).getProperties().get("name");
@@ -45,6 +56,9 @@ public class MainMenu extends ApplicationAdapter {
 						.getByType(RectangleMapObject.class).get(i).getRectangle();
 			}
 		}
+		pp = new Player(player.x,player.y,player.width,player.height, .5f);
+		pp.Create();
+
 		bounds = new Rectangle[map.getLayers().get("bounds").getObjects().getByType(RectangleMapObject.class).size];
 		for (int i=0;i<bounds.length;i++){
 			bounds[i] = map.getLayers().get("bounds").getObjects().getByType(RectangleMapObject.class).get(i).getRectangle();
@@ -55,23 +69,50 @@ public class MainMenu extends ApplicationAdapter {
 	public void render () {
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.end();
-		render.setView((OrthographicCamera) stage.getCamera());
+
+		isonground = false;
+		gravity += (10 * 9.8f)/2000;
+		player.y-=gravity;
+		for (int i = 0; i < bounds.length;i++){
+			if (bounds[i].overlaps(player)) {player.y+=gravity; gravity=0; isonground = true;}
+			}
+		if(Gdx.input.isKeyPressed(Input.Keys.D)){ player.x +=pp.movementSpeed;
+		for (int i = 0; i < bounds.length;i++){
+			if (bounds[i].overlaps(player))player.x-=pp.movementSpeed;
+		}}
+		if(Gdx.input.isKeyJustPressed(Input.Keys.SPACE)&&isonground){ player.y += 25f; }
+		if(Gdx.input.isKeyPressed(Input.Keys.A)){
+			player.x -=pp.movementSpeed;
+			for (int i = 0; i < bounds.length;i++){
+				if (bounds[i].overlaps(player))player.x+=pp.movementSpeed;
+			}}
+
+		cam.position.x = player.x;
+		cam.position.y = player.y;
+		cam.update();
+
+//		render.setView((OrthographicCamera) stage.getCamera());
 		render.render(ground);
+
+		stage.getBatch().setProjectionMatrix(cam.combined);
+		render.setView(cam);
 		stage.draw();
 		sr.begin(ShapeRenderer.ShapeType.Line);
-		sr.setColor(Color.RED);
+		sr.setColor(Color.BLUE);
+		batch.begin();
+		pp.xCenter = player.x;
+		pp.yCenter = player.y;
+		if (Gdx.input.isKeyPressed(Input.Keys.D)) {pp.isWalking = true;}
+		if (!Gdx.input.isKeyPressed(Input.Keys.D)) {pp.isWalking = false;}
+		if (pp.isWalking){
+			pp.DrawAnimation(batch);
+		} else pp.draw(batch);
+		batch.end();
 		sr.rect(player.x, player.y, player.width, player.height);
+		sr.setColor(Color.WHITE);
 		for (int i=0;i<bounds.length;i++){
 			sr.rect(bounds[i].x, bounds[i].y, bounds[i].width, bounds[i].height);
 		}
 		sr.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
 	}
 }
--- Obscurity/core/src/com/obscurity/game/Player.java
+++ Obscurity/core/src/com/obscurity/game/Player.java
@@ -1,25 +1,40 @@
 package com.obscurity.game;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.graphics.g3d.model.Animation;
-import com.badlogic.gdx.math.Rectangle;
+
 
 public class Player {
-    TextureRegion playerTextureRegion;
-    Animation animation;
     float xCenter,yCenter,width,height,movementSpeed;
-
-    public Player(TextureRegion playerTextureRegion,float xCenter, float yCenter, float width, float height, float movementSpeed){
-        this.playerTextureRegion = playerTextureRegion;
+    private float elapsedTime = 0f;
+    TextureAtlas textureAtlas;
+    Animation <TextureRegion> animation;
+    Texture texture;
+    boolean isWalking = false;
+    public Player(float xCenter, float yCenter, float width, float height, float movementSpeed){
         this.xCenter = xCenter;
         this.yCenter = yCenter;
         this.width = width;
         this.height = height;
         this.movementSpeed = movementSpeed;
     }
+    public void Create(){
+        texture = new Texture("HeroAnimations/Standing/hero1.png");
+        textureAtlas = new TextureAtlas(Gdx.files.internal("HeroAnimations/Walk/walk.atlas"));
+        animation = new Animation<TextureRegion>(1f/4f, textureAtlas.getRegions());
+
+    }
+    public void DrawAnimation(Batch batch){
+        elapsedTime += Gdx.graphics.getDeltaTime();
+        batch.draw(animation.getKeyFrame(elapsedTime,true),xCenter,yCenter,width,height);
+        isWalking = true;
+    }
     public void draw(Batch batch){
-        batch.draw(playerTextureRegion, xCenter, yCenter,width, height);
+        batch.draw(texture,xCenter,yCenter,width,height);
     }
 }
